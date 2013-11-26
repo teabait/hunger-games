8435f255c4a0f53119aa46dcf9ceedeaf79126f4
teabait <hello.teaho@gmail.com>
2013-11-25 22:28:11 -0500

    changed .first to .sample so now it's a random tribute

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,7 +2,7 @@ class Game < ActiveRecord::Base
   def reap
     i = 1
     while i < 3
-      tribute = Citizen.where(district_id: i).first
+      tribute = Citizen.where(district_id: i).sample
       Tribute.create(citizen_id: tribute.id, district_id: tribute.district_id)
       i += 1
     end
