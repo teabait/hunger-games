62d3f31279405c1c169ec124d80973412dcd97d1
teabait <hello.teaho@gmail.com>
2013-11-25 22:40:01 -0500

    game does not reap people outside of the age range

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,10 +1,12 @@
 class Game < ActiveRecord::Base
   def reap
     i = 1
-    while i < 4
+    while i < 5
       2.times do
         tribute = Citizen.where(district_id: i).sample
-        Tribute.create(citizen_id: tribute.id, district_id: tribute.district_id)
+        unless tribute.age > 18 || tribute.age < 12
+          Tribute.create(citizen_id: tribute.id, district_id: tribute.district_id)
+        end
       end
       i += 1
     end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,22 +4,24 @@ describe Game do
   let(:game) { Game.create(number: 1) }
   context 'citizens are in the database' do
     before do
-      citizen1 = Citizen.create(name: "Aaron", age: 17, sex: "M", district_id: 1)
-      citizen2 = Citizen.create(name: "Tara", age: 13, sex: "F", district_id: 1)
-      citizen1 = Citizen.create(name: "Remus", age: 12, sex: "M", district_id: 1)
-      citizen2 = Citizen.create(name: "Thor", age: 16, sex: "F", district_id: 1)
-      citizen1 = Citizen.create(name: "Rover", age: 17, sex: "M", district_id: 1)
-      citizen2 = Citizen.create(name: "Grover", age: 13, sex: "F", district_id: 1)
-      citizen1 = Citizen.create(name: "Dumont", age: 12, sex: "M", district_id: 1)
-      citizen2 = Citizen.create(name: "Liann", age: 16, sex: "F", district_id: 1)
-      citizen1 = Citizen.create(name: "Rumer", age: 17, sex: "M", district_id: 2)
-      citizen2 = Citizen.create(name: "Zola", age: 13, sex: "F", district_id: 2)
-      citizen1 = Citizen.create(name: "Sage", age: 12, sex: "M", district_id: 2)
-      citizen2 = Citizen.create(name: "Wash", age: 16, sex: "F", district_id: 2)
-      citizen1 = Citizen.create(name: "Monty", age: 17, sex: "M", district_id: 3)
-      citizen3 = Citizen.create(name: "Ordon", age: 13, sex: "F", district_id: 3)
-      citizen1 = Citizen.create(name: "Noodle", age: 13, sex: "M", district_id: 3)
-      citizen3 = Citizen.create(name: "Triash", age: 16, sex: "F", district_id: 3)
+      Citizen.create(name: "Aaron", age: 17, sex: "M", district_id: 1)
+      Citizen.create(name: "Tara", age: 13, sex: "F", district_id: 1)
+      Citizen.create(name: "Remus", age: 12, sex: "M", district_id: 1)
+      Citizen.create(name: "Thor", age: 16, sex: "F", district_id: 1)
+      Citizen.create(name: "Rover", age: 17, sex: "M", district_id: 1)
+      Citizen.create(name: "Grover", age: 13, sex: "F", district_id: 1)
+      Citizen.create(name: "Dumont", age: 12, sex: "M", district_id: 1)
+      Citizen.create(name: "Liann", age: 16, sex: "F", district_id: 1)
+      Citizen.create(name: "Rumer", age: 17, sex: "M", district_id: 2)
+      Citizen.create(name: "Zola", age: 13, sex: "F", district_id: 2)
+      Citizen.create(name: "Sage", age: 12, sex: "M", district_id: 2)
+      Citizen.create(name: "Wash", age: 16, sex: "F", district_id: 2)
+      Citizen.create(name: "Monty", age: 17, sex: "M", district_id: 3)
+      Citizen.create(name: "Ordon", age: 13, sex: "F", district_id: 3)
+      Citizen.create(name: "Noodle", age: 13, sex: "M", district_id: 3)
+      Citizen.create(name: "Triash", age: 16, sex: "F", district_id: 3)
+      Citizen.create(name: "Brack", age: 10, sex: "M", district_id: 4)
+      Citizen.create(name: "Florence", age: 36, sex: "F", district_id: 4)
     end
     describe '::new' do
       it 'creates a game without a problem' do
@@ -29,7 +31,7 @@ describe Game do
         expect(game.number).to eq(1)
       end
       it 'has citizens' do
-        expect((Citizen.all).count).to eq(16)
+        expect((Citizen.all).count).to eq(18)
       end
 
       describe '#reap' do
@@ -44,6 +46,10 @@ describe Game do
             expect(Tribute.where(district_id: 1).count).to eq(2)
             expect(Tribute.where(district_id: 2).count).to eq(2)
             expect(Tribute.where(district_id: 3).count).to eq(2)
+          end
+          it 'does not select people outside of the age range 12-18' do
+            game.reap
+            expect(Tribute.where(district_id: 4).count).to eq(0)
           end
         end
       end
